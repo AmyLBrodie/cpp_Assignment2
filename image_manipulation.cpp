9184d83e8431bf39f4a304a56f3dbe3effb2c8b0
Amy Brodie <brdamy004@myuct.ac.za>
2016-03-18 14:50:34 +0200

    Initial commit of Tutorial 2; driver.cpp, image_manipulation.cpp and image_manipulation.h have been created.

---
+++ image_manipulation.cpp
@@ -0,0 +1,35 @@
+/* 
+ * Author: Amy Brodie (BRDAMY004)
+ * CSC3002H Tutorial 2
+ * Created on 18 March 2016, 2:15 PM
+ */
+
+#include "image_manipulation.h"
+
+
+namespace BRDAMY004{
+    VolImage::VolImage() : width(0), height(0) {
+        
+    }
+    
+    bool VolImage::readImages(std::string baseName){
+        
+    }
+    
+    void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
+        
+    }
+    
+    void VolImage::extract(int sliceId, std::string output_prefix){
+        
+    }
+    
+    int VolImage::volImageSize(){
+        
+    }
+    
+    VolImage::~VolImage(){
+        
+    }
+    
+}
---
+++ image_manipulation.h
@@ -0,0 +1,35 @@
+
+/* 
+ * Author: Amy Brodie (BRDAMY004)
+ * CSC3002H Tutorial 2
+ * Created on 18 March 2016, 2:15 PM
+ */
+
+#ifndef IMAGE_MANIPULATION_H
+#define IMAGE_MANIPULATION_H
+
+namespace BRDAMY004{
+    class VolImage {
+    
+    private:
+       int width, height;
+       std::vector<unsigned char**> slices;
+       
+    public:
+        VolImage();
+        
+        ~VolImage();
+
+        bool readImages(std::string baseName);
+
+        void diffmap(int sliceI, int sliceJ, std::string output_prefix);
+
+        void extract(int sliceId, std::string output_prefix);
+
+        int volImageSize(void);
+    };
+}
+
+
+#endif /* IMAGE_MANIPULATION_H */
+
